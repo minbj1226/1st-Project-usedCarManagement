f46f501a43cf106bd753801f0884bce4463e1d06
minbj1226 <minbj1226@naver.com>
2025-10-27 09:25:46 +0900

    학원에서 commit

--- projectCarSell/src/teamproject2/RunTest.java
+++ projectCarSell/src/teamproject2/RunTest.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class RunTest {
 	
 	public static void main(String[] args) {
-//		OrderListManagerDesign olmd=new OrderListManagerDesign();
-		OrderListUserDesign olud=new OrderListUserDesign(2);
+		OrderListManagerDesign olmd=new OrderListManagerDesign();
+//		OrderListUserDesign olud=new OrderListUserDesign(2);
 	}
 }
